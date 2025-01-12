598cba5c883a8af2e3483c6362403a640a278da4
2acholsk1 <p.zacholski@wp.pl>
2025-01-12 19:14:15 +0100

    Edit convert onxx
    Edit converting script to using click and export first model

--- solapafsfh/cli/convert_onxx.py
+++ solapafsfh/cli/convert_onxx.py
@@ -1,27 +1,40 @@
-from solapafsfh.models.segmentation_model import SegmentationModel
+import yaml
 import torch.onnx
+import click
 
-model = SegmentationModel(
-        'UNet',
-        'resnet18',
-        3,
-        ['background', 'lawn', 'paving'],
-        'Dice',
-        1e-3
-    )
-model.load_state_dict(torch.load('checkpoints/best-checkpoint-v16.ckpt', map_location='cpu')['state_dict'])
-model.eval()
+from pathlib import Path
 
-x = torch.rand(1, 3, 512, 512)
-_ = model(x)
+from solapafsfh.models.segmentation_model import SegmentationModel
 
-torch.onnx.export(model,
-                x,
-                'onxx_model',
-                export_params=True,
-                opset_version=15,
-                input_names=['input'],
-                output_names=['output'],
-                do_constant_folding=False)
-checkpoint = torch.load('checkpoints/best-checkpoint-v16.ckpt', map_location='cpu', weights_only=True)
-model.load_state_dict(checkpoint['state_dict'])
+@click.command()
+@click.argument('checkpoint_path', type=click.Path(exists=True, path_type=Path))
+@click.option('--export_model_name', type=click.STRING, default='onxx_model')
+def export_model_to_onxx(checkpoint_path: Path, export_model_name: str):
+    with open("configs/config.yaml", "r") as file:
+        config = yaml.safe_load(file)
+
+    model = SegmentationModel(
+            config['model']['name'],
+            config['model']['encoder_name'],
+            config['model']['in_channels'],
+            config['model']['classes'],
+            config['model']['loss_func'],
+            config['model']['lr'],
+        )
+    model.load_state_dict(torch.load(checkpoint_path, map_location='cpu')['state_dict'])
+    model.eval()
+
+    x = torch.rand(1, 3, 512, 512)
+    _ = model(x)
+    export_model_name = export_model_name + '.onxx'
+    torch.onnx.export(model,
+                    x,
+                    'onxx_models/'+export_model_name,
+                    export_params=True,
+                    opset_version=15,
+                    input_names=['input'],
+                    output_names=['output'],
+                    do_constant_folding=False)
+
+if __name__ == '__main__':
+    export_model_to_onxx()
